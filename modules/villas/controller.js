22f065b5bd2dc165383d2c7d93caba8e9a2416f7
Ali-Script <ali.prg01@gmail.com>
2024-03-01 01:06:19 +0330

    villa model

--- modules/villas/controller.js
+++ modules/villas/controller.js
@@ -1,18 +1,35 @@
 const mongoose = require('mongoose');
-const userModel = require('./../auth/model');
-const validator = require("email-validator");
+const villaModel = require('./../auth/model');
 const joi = require("./../../validator/villaValidator");
 
 exports.add = async (req, res) => {
     try {
         const { address, map, cover, description, capAndSizeAndRooms, facility, sanitaryFacilities, timing, price, rules } = req.body;
-        // req.body = { address, map, cover, description, capAndSizeAndRooms, facility, sanitaryFacilities, timing, price, rules }
-
-        let a = [{ a: "tikjh" }]
 
         const validator = joi.validate(req.body)
         if (validator.error) return res.status(409).json({ message: validator.error.details })
-        return res.status(200).json("succ")
+
+        const ifDUPLC = await userModel.findOne(map)
+        if (ifDUPLC) {
+            return res.status(409).json({ message: "this location is already exist" })
+        }
+
+        const newVilla = await villaModel.create({
+            user: req.user._id,
+            address,
+            map,
+            cover: req.files.filename,
+            description,
+            capAndSizeAndRooms,
+            facility,
+            sanitaryFacilities,
+            timing,
+            price,
+            rules
+        })
+        if (!newVilla) return res.status(500).json({ message: "can not add data to the database" })
+
+        return res.status(200).json({ message: "Succ !" })
 
     } catch (err) { return res.status(422).send(err.message); }
 }
